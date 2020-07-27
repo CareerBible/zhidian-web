4d69dc84b1163a9c97117375c4449c432f334683
Liao Yazhi <1594435860@qq.com>
2020-07-27 20:14:09 +0800

    首页添加点击调用下拉

--- static/js/b.js
+++ static/js/b.js
@@ -13,7 +13,7 @@ var xhr = new XMLHttpRequest()
           for(var j=0; j<data.data[i].children.length; j++){
             var sub2 = ''
             for(var k=0; k<data.data[i].children[j].children.length; k++){
-              sub2 += '<li><a href="https://zhidian.dookbook.info/report/content/?code='+data.data[i].children[j].children[k].name+'">'+data.data[i].children[j].children[k].name+'</a></li>'
+              sub2 += '<li><a href="https://zhidian.dookbook.info/report/content/?code='+data.data[i].children[j].children[k].code+'">'+data.data[i].children[j].children[k].name+'</a></li>'
             }
             sub += '<li><svg viewbox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M328.555136 515.587823l364.265028-364.265028c20.07399-20.049007 20.07399-52.553369 0-72.577393-20.024024-20.049007-52.528386-20.049007-72.577393 0L220.992263 477.971128c-20.024024 20.024024-20.024024 52.528386 0 72.577393 1.578929 1.578929 3.383306 2.83188 5.112534 4.160179 0.300796 0.325779 0.526243 0.701724 0.827039 1.027503l389.877663 389.877663c19.57293 19.57293 51.300418 19.57293 70.873148 0s19.57293-51.300418 0-70.873148L328.555136 515.587823z" fill="#bbb"></path></svg>'+data.data[i].children[j].name+'<ul>'+sub2+'</ul></li>'
           }
--- static/js/index.js
+++ static/js/index.js
@@ -32,11 +32,33 @@ text.oninput = function (event) {
   }
 }
 
-text.onclick = function () {
+text.onclick = function (event) {
+  event.stopPropagation()
   if(this.value == '') {
     xiala.style.display = "none"
   } else {
     xiala.style.display = "block"
+    console.log(this.value)
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', HTTP_QZ + '/api/edu/university/disciplines/?q=' + this.value)
+    xhr.send()
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        var data = JSON.parse(xhr.responseText)
+        if(data.code === 0) {
+          if(data.hints == null || data.hints.length === 0){
+            xiala.innerHTML = '<li><a href="#" class="search-results">暂无搜索结果</a></li>'
+          } else {
+            xiala.innerHTML = ''
+            for(var i=0;i<data.hints.length;i++){
+              xiala.innerHTML += '<ul><li><a href="https://zhidian.dookbook.info/report/content/?code='+data.hints[i].code+'">'+data.hints[i].name+'</a></li></ul>'
+            }
+          }
+        } else {
+          console.debug('fetchSearchHintList: connecting')
+        }
+      }
+    }
   }
 }
 
