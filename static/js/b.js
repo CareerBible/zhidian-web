bba881a79698ce4057b8b34a969a40b5c3b0cd42
Liao Yazhi <1594435860@qq.com>
2020-07-28 17:37:20 +0800

    添加返回没有历史上一页操作

--- static/js/b.js
+++ static/js/b.js
@@ -59,5 +59,30 @@ var xhr = new XMLHttpRequest()
 var shangyiye = document.querySelector('.shangyiye')
 
 shangyiye.onclick = function () {
-  window.history.go(-1); 
+  goBack()
+}
+
+function goBack(){
+  if ((navigator.userAgent.indexOf('MSIE') >= 0) && (navigator.userAgent.indexOf('Opera') < 0)){ // IE
+    if(history.length > 0){
+      window.history.go( -1 );
+    }else{
+      window.location.href = "/";
+    }
+  }else{ //非IE浏览器
+    if (navigator.userAgent.indexOf('Firefox') >= 0 ||
+      navigator.userAgent.indexOf('Opera') >= 0 ||
+      navigator.userAgent.indexOf('Safari') >= 0 ||
+      navigator.userAgent.indexOf('Chrome') >= 0 ||
+      navigator.userAgent.indexOf('WebKit') >= 0){
+
+      if(window.history.length > 1){
+        window.history.go( -1 );
+      }else{
+        window.location.href = "/";
+      }
+    }else{ //未知的浏览器
+      window.history.go( -1 );
+    }
+  }
 }
