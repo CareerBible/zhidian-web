65e16f4d2100d43e8c26bfaba025cfa95b006b2b
Zhong Yitian <363778748@qq.com>
2020-08-27 09:56:03 +0800

    初始0.0.3

---
+++ static/js/common.js
@@ -0,0 +1,85 @@
+
+
+window.onload = function(){
+    axios.defaults.headers.common["uid"] = '56ed7379da47434292deeb8d472ebb0c';
+    //ajax方法
+    function ajax (method, url, params, fun) {
+    method = method.toUpperCase()            //在传入method的时候可以忽略大小写
+    var xhr = new XMLHttpRequest()
+    if (typeof params === 'object') {        //如果在地址传入的东西是一个对象，我们将它的格式转化为urlencoded
+      var tempArr = []
+      for (var key in params) {
+        var value = params[key]
+        tempArr.push(key + '=' + value)
+      }
+      params = tempArr.join('&') //===>这里的格式就为parsms=[key1=value&key2=value2]
+    }
+    if (method === 'GET') {
+      url += '?' + params
+    }
+    xhr.open(method, url, false)
+    var data = null
+    if (method === 'POST') {    //如果请求的方式为POST，需要手动设置请求头的Content-Type
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+      data = params
+    }
+    xhr.onreadystatechange = function () {
+      if (this.readyState !== 4) return
+      // console.log(this.responseText)  //这里可以得到响应体，但是后面要处理的内容应该根据使用者的需要来处理
+      fun(this.responseText)
+    }
+    xhr.send(data)
+  }
+
+    //获取url中"?"符后的字串
+    function GetRequest() { 
+      var url = location.search; 
+      var theRequest = new Object();
+      if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for(var i = 0; i < strs.length; i ++) {
+        theRequest[strs[i].split("=")[0]]=(strs[i].split("=")[1]);
+        }
+      }
+      return theRequest;
+    }
+
+    //判断是否存有uid
+    function isLogin(){
+      var uid = window.localStorage.getItem('uid'),
+          openid = window.localStorage.getItem('openid'),
+          skipUrl = '',
+          url = 'https://zhidian.dookbook.info/api/wechat/getAuthorizeCodeUrl';
+      if(uid === null && openid === null){//没有用户id
+        ajax('get', url,{},function (res) {
+            var resData = JSON.parse(res);
+            if(resData.code == 200){
+              skipUrl = resData.data.url;
+              // window.location.href = skipUrl + 'index.html';
+            }
+        })
+      }
+    }
+    isLogin();
+
+    //得到code再获取用户信息
+    function getUserInfo(){
+      var obj = GetRequest(),
+          Code = obj['code'],
+          url = 'https://zhidian.dookbook.info/api/wechat/getAuthorizeCodeUrl';
+      ajax('get', url,{
+        'code': Code
+      },function (res) {
+          var resData = JSON.parse(res);
+          if(resData.code == 200){
+            var data = resData.data;
+            window.localStorage.setItem('uid', data.id);
+            window.localStorage.setItem('openid', data.openid);
+            window.localStorage.setItem('unionid', data.unionid);
+          }
+      })
+    }
+}
+
+ 
---
+++ static/js/salary.js
@@ -0,0 +1,399 @@
+ //数据
+ let data = {
+    domain: 'https://zhidian.dookbook.info',  //域名
+    showSearch: false,
+    searchTxt: '', //搜索框文字
+    area: '全国',
+    districtId: 0,//获取被选中的值，默认选中是0（全国）
+    areaList: [],
+    cityList:[],
+    professList:[
+      {
+        id: '',
+        name: '',
+        code: ''
+      }
+    ],
+    showChart: false,  //是否显示对比图表
+    showLogin: false,   //显示“加载中”
+    showLoading: false,   //显示“正在加载” 
+    showNoData: false,   //显示“没有数据”
+    showProvince: false,  //显示省
+    showCity: false,  //显示市
+    showBackBtn: false, //显示TOP按钮
+    showMask: false,
+    showPop: false,
+    showSuccess: false,
+    jobCode: '010101',
+    page: 1,  //页码
+    limit: '5', //每页显示多少条数据
+    professionAvg: 8000,
+    container: null,// 绑定能被监听滚动的元素
+    domHeight: 0,// 内容可视区的高度
+    professionSalaryList: [
+      {
+        years: '1年以下',
+        salary: '2.5'
+      },
+      {
+        years: '1-3年',
+        salary: '4.5'
+      },
+      {
+        years: '3-5年',
+        salary: '8.5'
+      },
+      {
+        years: '5-10年',
+        salary: '10'
+      },
+      {
+        years: '10年以上',
+        salary: '15'
+      }
+    ],
+    jobList: [],
+    totalPage: 0,  //总页数
+    Dom: '',
+    clientH: '',
+    note: '',
+    salaryChart: null,
+    dataArr:[],
+    profession: document.querySelector('#profession'),
+    chartOption: {
+      color: ['#516b91','#59c4e6','#edafda','#93b7e3','#a5e7f0'],
+      tooltip: {
+          trigger: 'axis'
+      },
+      legend: {
+          data: []
+      },
+      grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+      },
+      xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['1年以下', '1-3年', '3-5年', '5-10年', '10年以上'],
+          axisLine:{
+            lineStyle:{
+                color:'#888'
+            }
+          } 
+      },
+      yAxis: {
+          type: 'value',
+          axisLabel: {
+              formatter: '{value} k'
+          },
+          axisLine:{
+            lineStyle:{
+                color:'#888'
+            }
+          } 
+      },
+      series: []
+    }
+}
+
+//Vm实例
+var vm = new Vue({
+    el: "#salaryAnalysis",    //挂载元素
+    data: data,
+    mounted: function(){
+        // this.login();
+        this.salaryChart = echarts.init(document.getElementById('chart')); 
+        this.$nextTick(function() {  
+          this.clientH = document.documentElement.clientHeight;
+          this.Dom = document.getElementById('salaryAnalysis');
+          //this.userId = window.localStorage.getItem('uid');
+          //axios.defaults.headers.common["uid"] = this.userId;
+        })
+        //初始"哲学"数据
+        this.getProfession('010101', false);
+    },
+    methods: {  //  放方法函数
+        getAreaOption: function(){  //获取地区选项
+            var url = this.domain + '/api/district/list';
+            const that = this;
+            axios.get(url).then(function(res) {
+                var resData = res.data;
+                if(resData.code === 200){
+                    that.showProvince = true;
+                    that.areaList = resData.data.list;
+                    for(var i = 0; i<that.areaList.length; i++){
+                      that.areaList[i].show = false;
+                    }
+                }
+            });
+        },
+        selecteProvince: function(item){ //选中省
+          item.show = true;
+          this.showCity = item.show;
+          this.cityList = item.listChild;
+          for(var i = 0; i < this.cityList.length; i++){
+            this.cityList.show = false;
+          }
+        },
+        selectCity: function(item){//选中市
+          this.showProvince = false;
+          this.showCity = false;
+          this.districtId = item.id;
+          this.area = item.name;
+        },
+        getSearchTxt: function(){ //按照专业名称搜索文字获取专业列表
+            var reg = new RegExp("[\\u4E00-\\u9FFF]+","g");
+            if(!reg.test(this.searchTxt)){    //非中文
+               return;
+            }
+            var url = this.domain + '/api/discipline/list';
+            const that = this;
+            if(this.searchTxt){
+                var params = {'search': this.searchTxt};
+                axios.get(url,{
+                    params:params
+                }).then(function(res) {
+                    var resData = res.data;
+                    if(resData.code === 200){
+                        that.professList = resData.data.list;
+                        that.professList.length == 0 ? that.showSearch = false:that.showSearch = true;
+                    }
+                });
+            }
+        },
+        getProfession: function(code, onOff){ //选择专业获取行业数据
+            this.searchTxt = '';
+            this.jobCode = code;
+            var url = this.domain + '/api/statistical/listDiscipline';
+            const that = this;
+            var params = {
+              disciplineCode: this.jobCode,
+              limit: this.limit,
+              page: this.page
+            };
+            if(this.districtId != 0){
+               params.districtId = this.districtId; 
+            }
+            
+            axios.get(url,{params: params}).then(function(res) {
+                var resData = res.data;
+                if(resData.code === 200){
+                    that.showSearch = false;
+                    var arr = resData.data.listDisciplineAvgSalaryWorkingYears;
+                    var newArr = [];
+                    for (var i = 0; i <= arr.length;i++) {
+                      newArr.push(arr.pop());
+                    }
+                    newArr.push(arr[0]);
+                    for(var i = 0; i < newArr.length; i++){
+                      that.professionSalaryList[i].years = newArr[i].workingyears;
+                      that.professionSalaryList[i].salary = newArr[i].disciplineavgsalaryworkingyears.toFixed(1);
+                    }
+                }
+            });
+            this.getJodData(true);
+            if(onOff){
+                var time = null;
+                time = setTimeout(function(){
+                    that.showMask = true;
+                    that.showPop = true;
+                }, 10000);
+            }
+        },
+        getJodData: function(isSearch){ //获取岗位数据
+            var url = this.domain + '/api/statistical/listPage';
+            const that = this;
+            var params = {
+              disciplineCode: this.jobCode,
+              limit: this.limit,
+              page: this.page
+            };
+            if(this.districtId != 0){
+               params.districtId = this.districtId; 
+            }
+            axios.get(url,{params: params}).then(function(res) {
+                var resData = res.data;
+                if(resData.code === 200){
+                    that.totalPage = parseInt(resData.data.totaCount/that.limit);
+                    var arr = resData.data.listPositionAvgSalary;
+                    if(arr.length == 0){
+                        that.showNoData = true;
+                        that.note = "暂无数据";
+                    }else{
+                        if(isSearch){   //是否搜索来的
+                            that.jobList = [];
+                        }
+                        for(var i = 0; i < arr.length; i++){
+                            that.$set(arr[i], 'compareBtn', true);
+                            that.jobList.push(arr[i]);
+                        }
+                    }
+                }else {
+                  that.showNoData = true;
+                  that.note = "访问失败";
+                }
+            });
+        },
+        turnPage: function(){ 
+          if(this.page == this.totalPage){ //没有剩余页数
+            this.showLogin = false;
+            return;
+          }else {
+            this.showLogin = true;
+            this.page++;
+            setTimeout(this.getJodData(false),500);
+          }
+        },
+        viewmore: function() {  //加载更多
+            var domH = this.clientH;
+            var domScrollH = this.Dom.scrollHeight;
+            var domScrollTop = Math.ceil(this.Dom.scrollTop);
+            var scrollArea = parseInt(domScrollH - domH);
+            this.setFixed();
+            if(this.Dom.scrollTop > 600){
+                this.showBackBtn = true
+            }else {
+                this.showBackBtn = false
+            }
+            console.log(this.Dom.scrollTop, this.showBackBtn);
+            if(domScrollTop == scrollArea){
+                this.turnPage();
+            }else {
+                this.showLogin = false;
+            }
+        },
+        compareOrCancel: function(item){  //对比/取消按钮
+          if(item.compareBtn){ //加对比
+            if(this.chartOption.legend.data.length == 6){
+                alert('最多只能对比五个职位');
+                return;
+            }
+            this.addRecord(item);
+            item.compareBtn = false;
+          }else {//取消
+            this.removeRecord(item);
+            item.compareBtn = true;
+          }
+        },
+        addRecord: function(item){ //添加图表数据
+            var avg = item.listPositionAvgSalaryScope,arr = this.chartOption.series;
+            this.dataArr = [];
+            for(var i = 0; i < avg.length; i++){
+              var num = parseFloat((avg[i].positionavgsalarymax + avg[i].positionavgsalarymin)/2).toFixed(1);
+              this.dataArr.push(num);
+            }
+            this.chartOption.legend.data.push(item.name);  //职业名称
+
+            if(arr.length == 0){
+                this.addProfessionAvg();    //添加行业平均薪资
+                this.addFn(item, this.dataArr);
+                this.showChart = true;
+                return;
+            }
+            for(var i = 0; i < arr.length; i++){ //薪资数据
+              if(arr[i].name == ''){
+                this.chartOption.series[i] = {
+                  name: item.name,
+                  data: this.dataArr,
+                  type: 'line'
+                }
+                return;
+              }
+            }
+            this.addFn(item, this.dataArr);
+            
+        },
+        addProfessionAvg: function(){   //对比图添加行业平均数据
+            var arr = this.professionSalaryList, salaryLi = [];
+            for(var i = 0; i < arr.length; i++){
+                salaryLi.push(arr[i].salary);
+            }
+            this.chartOption.legend.data.push('行业平均');
+            this.chartOption.series.push({
+                name: '行业平均',
+                data: salaryLi,
+                type: 'line'
+            });
+        },
+        addFn: function(item,arr){
+          this.chartOption.series.push({
+            name: item.name,
+            data: arr,
+            type: 'line'
+          });
+        },
+        removeRecord: function(item){ //删除对应数据
+          var arr = this.chartOption.legend.data,
+              index = arr.indexOf(item.name);
+          this.chartOption.legend.data.splice(index, 1);//去掉图表对应名称
+          this.chartOption.series[index] = {
+              name: '',
+              data: [],
+              type: 'line'
+          }
+          if(this.chartOption.legend.data.length == 1){
+                this.chartOption.legend.data = [];
+                this.chartOption.series = []
+                this.showChart = false;
+          }
+        },
+        setFixed: function(){//固定图表
+            if(!this.showChart){return;}
+          var scrollTop = this.Dom.scrollTop;
+          if(scrollTop > 120){
+              this.profession.style.position = 'fixed'
+              this.profession.style.top = '0'
+              this.profession.style.width = 'calc(100% - 80px)'
+              this.profession.style.zIndex = '3'
+              this.profession.querySelector('section').style.marginTop = '0'
+          } else {
+                this.profession.style.position = 'static'
+                this.profession.querySelector('section').style.marginTop = '40px'
+                this.profession.style.width = '100%'
+          }
+        },
+        backTop: function(){  //回到顶部
+          var time = null,that = this;
+          time = setInterval(function(){
+            var scrollTop = that.Dom.scrollTop;
+            if(scrollTop === 0){
+              clearInterval(time);
+            }
+            that.Dom.scrollTop = scrollTop - 100;
+          }, 16);
+        },
+        closePop: function(){   //未支付关闭弹窗
+            this.showMask = false;
+            this.showPop = false;
+            //初始"哲学"数据
+            this.getProfession('010101', false);  //回到哲学数据  
+            if(this.showChart){//图表关闭
+                this.chartOption.legend.data = [];
+                this.chartOption.series = []
+                this.showChart = false; 
+            }
+            this.profession.style.position = 'static';
+            this.profession.querySelector('section').style.marginTop = '40px'
+            this.profession.style.width = '100%'
+        }
+    },
+    watch: {
+      'chartOption': {
+        handler: function(newVal, oldVal) {
+          if (this.salaryChart) {
+            if (newVal) {
+              this.salaryChart.setOption(newVal);
+            } else {
+              this.salaryChart.setOption(oldVal);
+            }
+          } else {
+            this.init();
+          }
+        },
+        deep: true //对象内部属性的监听，关键。
+      }
+    }
+})
