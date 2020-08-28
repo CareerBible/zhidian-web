1192e08c6694103f16c138366731a9cf81468721
Zhong Yitian <363778748@qq.com>
2020-08-28 14:50:42 +0800

    地区搜索后title修改

--- static/js/salary.js
+++ static/js/salary.js
@@ -110,10 +110,10 @@ var vm = new Vue({
           that.clientH = document.documentElement.clientHeight;
           that.Dom = document.getElementById('salaryAnalysis');
           window.document.title = that.titleName;
-          // that.userId = '56ed7379da47434292deeb8d472ebb0c';
-          that.userId = window.localStorage.getItem('uid');
+          that.userId = '56ed7379da47434292deeb8d472ebb0c';
+          // that.userId = window.localStorage.getItem('uid');
           if(!that.userId){
-            window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+            // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
           }else{
             axios.defaults.headers.common["uid"] = that.userId;
             that.getProfession('010101', false, '哲学');//初始"哲学"数据
@@ -161,8 +161,8 @@ var vm = new Vue({
           this.showCity = false;
           this.districtId = item.id;
           this.area = item.name;
-          this.titleName = item.name;
           window.document.title = this.titleName;
+          console.log(this.titleName,document.title);
           this.getProfession(this.jobCode, false, this.searchTxt);
         },
         getSearchTxt: function(){ //按照专业名称搜索文字获取专业列表
@@ -201,6 +201,8 @@ var vm = new Vue({
 
             
             if(onOff){
+              this.titleName = name;
+              window.document.title = this.titleName;
               this.search+=1;
               if(this.search <= 1){
                 var time = null;
@@ -217,8 +219,6 @@ var vm = new Vue({
             axios.get(url,{params: params}).then(function(res) {
                 var resData = res.data;
                 if(resData.code === 200){
-                    that.titleName = name;
-                    window.document.title = that.titleName;              
                     that.showSearch = false;
                     that.professionAvg = (resData.data.disciplineAvgSalary * 1000);
                     var arr = resData.data.listDisciplineAvgSalaryWorkingYears;
