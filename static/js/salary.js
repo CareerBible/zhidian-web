178ac5580ce380e4a614b67bd489f7a9c02a3797
Zhong Yitian <363778748@qq.com>
2020-08-29 19:28:31 +0800

    clientH测试

--- static/js/salary.js
+++ static/js/salary.js
@@ -129,7 +129,7 @@ var vm = new Vue({
           if(isAndroid){
             this.clientH = document.documentElement.clientHeight-10;
           }else if(isiOS){
-            this.clientH = document.documentElement.clientHeight;
+            this.clientH = document.documentElement.clientHeight+10;
           }
         },
         getAreaOption: function(){  //获取地区选项
