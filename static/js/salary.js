d64c162176c39f0850d1274b6d6d746fb4e12ff4
Zhong Yitian <363778748@qq.com>
2020-08-29 18:05:49 +0800

    title修改

--- static/js/salary.js
+++ static/js/salary.js
@@ -108,13 +108,13 @@ var vm = new Vue({
         this.$nextTick(function() { 
           this.iosOrAndroid();
           this.Dom = document.getElementById('salaryAnalysis');//获取页面DOM的id
-          window.document.title = this.titleName; //初始页面title
           // this.userId = '56ed7379da47434292deeb8d472ebb0c';
           this.userId = window.localStorage.getItem('uid');
           if(!this.userId){
             window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
           }else{
             axios.defaults.headers.common["uid"] = this.userId;
+            window.document.title = this.titleName; //初始页面title
             this.getProfession('010101', false, '哲学');//初始"哲学"数据
             this.salaryChart = echarts.init(document.getElementById('chart')); 
           }
