dda472506fe4fc9fe45550053909f9cf5d431aa6
Zhong Yitian <363778748@qq.com>
2020-09-05 12:29:17 +0800

    微信分享url修改

--- static/js/salary.js
+++ static/js/salary.js
@@ -123,12 +123,12 @@ var vm = new Vue({
           this.clientH = document.documentElement.clientHeight;
           this.Dom = document.getElementById('salaryAnalysis');//获取页面DOM的id
           this.referreId = this.getQueryVariable('referreId');//获取推广id
-          if(this.referreId){window.sessionStorage.setItem('referreId',referreId)}
+          if(this.referreId){window.sessionStorage.setItem('referreId',this.referreId)}
           this.getRatioArr();
-          // this.userId = '56ed7379da47434292deeb8d472ebb0c';
-          this.userId = window.localStorage.getItem('uid');
+          this.userId = '56ed7379da47434292deeb8d472ebb0c';
+          // this.userId = window.localStorage.getItem('uid');
           if(!this.userId){
-            window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+            // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
           }else{
             axios.defaults.headers.common["uid"] = this.userId;
             window.document.title = this.titleName; //初始页面title
