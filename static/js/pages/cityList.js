cb6b6513bd774adedb912a5aa78c2d0c4ed0cbd6
Zhong Yitian <363778748@qq.com>
2020-09-10 14:55:04 +0800

    雷达图数据交互

--- static/js/pages/cityList.js
+++ static/js/pages/cityList.js
@@ -84,10 +84,10 @@ var vm = new Vue({
         this.$nextTick(function() {
             this.clientH = document.documentElement.clientHeight;
             this.Dom = document.getElementById('cityList');//获取页面DOM的id
-            this.userId = '56ed7379da47434292deeb8d472ebb0c';
-            // this.userId = userId();
+            // this.userId = '56ed7379da47434292deeb8d472ebb0c';
+            this.userId = userId();
             if(!this.userId){
-                // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+                window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
             }else{
                 axios.defaults.headers.common["uid"] = this.userId;
                 var id = getQueryVariable('professionId'), name = getQueryVariable('professionName');
