dca5d2ce1f197d8d855fcc2f4d36452186701bd7
Zhong Yitian <363778748@qq.com>
2020-09-11 09:54:05 +0800

    统一修改用户名获取

--- static/js/interceptor.js
+++ static/js/interceptor.js
@@ -1,8 +1,8 @@
 //获取用户名
 userId();
 function userId(){
-    var userId = '56ed7379da47434292deeb8d472ebb0c';
-    // var userId = window.localStorage.getItem('uid');
+    // var userId = '56ed7379da47434292deeb8d472ebb0c';
+    var userId = window.localStorage.getItem('uid');
   
     if(!userId){
         window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
