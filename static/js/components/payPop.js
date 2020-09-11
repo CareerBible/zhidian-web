3e5b113fcb602e81f8b3f991e63e04f7ca7fdb79
Zhong Yitian <363778748@qq.com>
2020-09-11 17:30:19 +0800

    支付页面+支付弹窗样式

--- static/js/components/payPop.js
+++ static/js/components/payPop.js
@@ -1,31 +1,12 @@
 
 Vue.component('paypop', {
-    template: '<div class="pop-box"  v-if="showPop">'+
-    '<div class="pop-ups">'+
-      '<p>9月限定<span>体验价</span></p>'+
-      '<p><span>1元</span>/月</p>'+
-      '<p><del>原价：9.9元/月</del></p>'+
-      '<p><span></span><em>高薪职业，从职典开始</em><span></span></p>'+
-      '<ul>'+
-        '<li>'+
-          '<p><img src="/static/img/icon-03.svg" alt=""></p>'+
-          '<p>查对口职业</p>'+
-        '</li>'+
-        '<li>'+
-          '<p><img src="/static/img/icon-04.svg" alt=""></p>'+
-          '<p>查职业薪资</p>'+
-        '</li>'+
-        '<li>'+
-          '<p><img src="/static/img/icon-05.svg" alt=""></p>'+
-          '<p>看职业对比</p>'+
-        '</li>'+
-      '</ul>'+
-      '<button @click="payFor">立即开通</button>'+
-      '<p class="logo"><img src="/static/img/logo.png" alt="">职典</p>'+
+    template: '<section class="payPop" v-if="showPop">'+
+    '<div>'+
+        '<p><em>解锁所有数据</em>每天2分钱 · 原价：<span>30元/月</span></p>'+
+        '<p><span><em>9.9</em>元/年</span>找个好工作，年薪10万起</p>'+
     '</div>'+
-    '<span class="xian"></span>'+
-    '<img src="/static/img/shut-down.svg" alt="" class="shut-down" @click="closePop">'+
-  '</div>',
+    '<button @click="goToPayFor">9月求职季特惠 · 立即购买</button>'+
+'</section>',
     data: function(){
         return {
             userId: '',
@@ -45,69 +26,8 @@ Vue.component('paypop', {
         })
     },
     methods: {
-        payFor: function(){
-            var url = domain() + '/api/pay/initPay';
-            const that = this;
-            var data = {
-              userId: this.userId,
-              tradeType: 'JSAPI'
-            }
-            axios.post(url,data).then(function(res) {
-                var resData = res.data;
-                if(resData.code === 200){
-                  var payForData = resData.data;
-                  that.payForToWx(payForData);
-                }
-            })
-        },
-        payForToWx: function(data){//微信支付
-            const that = this;
-            WeixinJSBridge.invoke('getBrandWCPayRequest', {
-                "appId": data.appId,
-                "timeStamp": data.timeStamp,
-                "nonceStr": data.nonceStr,
-                "package": data.packageStr,
-                "signType": data.signType,
-                "paySign": data.paySign 
-            },function(res) {
-                if (res.err_msg == "get_brand_wcpay_request:ok") {
-                    that.showPop = false;
-                    this.$emit('close', false);
-                    this.$emit('init', false);
-                    that.getOrder(data.outTradeNo);//支付成功后查询订单
-                }else {
-                    that.closePop();
-                }
-            });
-            if (typeof WeixinJSBridge == "undefined") {
-                if (document.addEventListener) {
-                    document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-                } else if (document.attachEvent) {
-                    document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-                    document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-                }
-            } else {
-                that.payForToWx();
-            }
-        },
-        getOrder: function(orderCode){ //查询订单
-            var url = domain() + '/api/orderQuery/order';
-            const that = this;
-            var params = {"outTradeNo": orderCode};
-            axios.get(url,{params: params}).then(function(res) {
-              var resData = res.data;
-              if(resData.code === 200){
-                var isVip = resData.data.isVip;
-                if(isVip){
-                  // that.showMask = true;
-                  // that.showSuccess = true;
-                }
-              }
-            })
-        },
-        closePop: function(){   //关闭支付弹窗
-            this.$emit('close', false);
-            this.$emit('init', true);
+        goToPayFor: function(){//跳转支付页面
+            window.location.href = '/payFor.html';
         }
     },
     watch: {
--- static/js/pages/payFor.js
+++ static/js/pages/payFor.js
@@ -1,14 +1,71 @@
-var data = {}
-
 var vm = new Vue({
     el: "#payFor",    //挂载元素
-    data: data,
+    data: {
+        userId: ''
+    },
     mounted: function(){
         this.$nextTick(function() {
-                    
+            this.userId = window.localStorage.getItem('uid');
         })
     },
     methods: {
-        
+        goToPay: function(){    //获取支付签名
+            var url = domain() + '/api/pay/initPay';
+            const that = this;
+            var data = {
+              userId: this.userId,
+              tradeType: 'JSAPI'
+            }
+            axios.post(url,data).then(function(res) {
+                var resData = res.data;
+                if(resData.code === 200){
+                  var payForData = resData.data;
+                  that.payForToWx(payForData);
+                }
+            })
+        },
+        payForToWx: function(data){//微信支付
+            const that = this;
+            WeixinJSBridge.invoke('getBrandWCPayRequest', {
+                "appId": data.appId,
+                "timeStamp": data.timeStamp,
+                "nonceStr": data.nonceStr,
+                "package": data.packageStr,
+                "signType": data.signType,
+                "paySign": data.paySign 
+            },function(res) {
+                if (res.err_msg == "get_brand_wcpay_request:ok") {
+                    alert('支付成功');
+                    that.getOrder(data.outTradeNo);//支付成功后查询订单
+                }else {
+                    alert('支付失败');
+                }
+            });
+            if (typeof WeixinJSBridge == "undefined") {
+                if (document.addEventListener) {
+                    document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                } else if (document.attachEvent) {
+                    document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                    document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                }
+            } else {
+                that.payForToWx();
+            }
+        },
+        getOrder: function(orderCode){ //查询订单
+            var url = domain() + '/api/orderQuery/order';
+            const that = this;
+            var params = {"outTradeNo": orderCode};
+            axios.get(url,{params: params}).then(function(res) {
+              var resData = res.data;
+              if(resData.code === 200){
+                var isVip = resData.data.isVip;
+                // if(isVip){
+                  // that.showMask = true;
+                  // that.showSuccess = true;
+                // }
+              }
+            })
+        }
     }
 })
