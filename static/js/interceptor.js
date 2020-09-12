52858e360061ebfa7728d66423c9a364a24b879d
Zhong Yitian <363778748@qq.com>
2020-09-12 12:11:21 +0800

    修复数据后台逻辑

--- static/js/interceptor.js
+++ static/js/interceptor.js
@@ -2,9 +2,13 @@
 userId();
 function userId(){
     // var userId = '56ed7379da47434292deeb8d472ebb0c';
-    var userId = window.localStorage.getItem('uid');
-  
+    var userId = localStorage.getItem('uid');
+    
     if(!userId){
+        var referreId = getQueryVariable('referreId');
+        if(referreId){
+            sessionStorage.setItem('referreId', referreId);
+        }
         window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb3417997b07e0f2e&redirect_uri=https%3A%2F%2Fzhidian.dookbook.info%2Fwx_auth.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
     }else{
         axios.defaults.headers.common["uid"] = userId;
